683831b61480a7e888cfe781a02bf66916663c4f
mohamed slaoui <mohamed16slaoui@gmail.com>
2025-08-31 22:09:15 +0100

    update

--- server.js
+++ server.js
@@ -1,49 +1,23 @@
-// server.js
 const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Serve frontend (index.html)
+const quotes = [
+    { text: "The only way to do great work is to love what you do.", author: "Steve Jobs" },
+    { text: "Life is what happens when you're busy making other plans.", author: "John Lennon" },
+    { text: "The journey of a thousand miles begins with one step.", author: "Lao Tzu" },
+    { text: "That which does not kill us makes us stronger.", author: "Friedrich Nietzsche" },
+    { text: "Be the change that you wish to see in the world.", author: "Mahatma Gandhi" },
+    { text: "It is never too late to be what you might have been.", author: "George Eliot" },
+    { text: "Everything you can imagine is real.", author: "Pablo Picasso" },
+    { text: "Whatever you are, be a good one.", author: "Abraham Lincoln" }
+];
+
+// Serve frontend
 app.use(express.static(path.join(__dirname, 'frontend')));
 
-const quotes = [
-            {
-                text: "The only way to do great work is to love what you do.",
-                author: "Steve Jobs"
-            },
-            {
-                text: "Life is what happens when you're busy making other plans.",
-                author: "John Lennon"
-            },
-            {
-                text: "The journey of a thousand miles begins with one step.",
-                author: "Lao Tzu"
-            },
-            {
-                text: "That which does not kill us makes us stronger.",
-                author: "Friedrich Nietzsche"
-            },
-            {
-                text: "Be the change that you wish to see in the world.",
-                author: "Mahatma Gandhi"
-            },
-            {
-                text: "It is never too late to be what you might have been.",
-                author: "George Eliot"
-            },
-            {
-                text: "Everything you can imagine is real.",
-                author: "Pablo Picasso"
-            },
-            {
-                text: "Whatever you are, be a good one.",
-                author: "Abraham Lincoln"
-            }
-        ];
-
-// API route
-
+// API endpoint
 app.get('/api/quotes', (req, res) => {
     res.json(quotes);
 });
