39d19601f5ff12c5d4c352f809d0da320a06afb4
mohamed slaoui <mohamed16slaoui@gmail.com>
2025-08-31 11:49:32 +0100

    new changes to production

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname)));
 
 // API route
 app.get('/', (req, res) => {
-    res.json({ message: 'Hello from Node.js ! Lets learn DevOps' });
+    res.json({ message: 'New changes to production' });
 });
 app.get('/api', (req, res) => {
     res.json({ message: 'Hello from Node.js backend API!' });
